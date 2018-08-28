dc4dd802d86c80fdeea68aa9cbe91216a08da566
Chris Franklin <cf73021@gmail.com>
2018-08-28 02:10:50 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,6 +8,6 @@ def create_project_hash
   binding.pry
   # projects: kickstarter.css("li.project.grid_4")
   # title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
+  # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
 end
